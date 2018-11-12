ffed79f5b9cde405f7120b13e52d86bf5ccd9c2e
Ricardo Alzamora Valle <alzamoraricardo5@gmail.com>
2018-11-11 22:43:16 -0500

    Estructura de projecto base para el sistema de parqueo de vehículos.

---
+++ src/main/java/com/is/sistemaparqueovehiculos/SistemaParqueoVehiculosApplication.java
@@ -0,0 +1,12 @@
+package com.is.sistemaparqueovehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaParqueoVehiculosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaParqueoVehiculosApplication.class, args);
+    }
+}
---
+++ src/test/java/com/is/sistemaparqueovehiculos/SistemaParqueoVehiculosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.is.sistemaparqueovehiculos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SistemaParqueoVehiculosApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
