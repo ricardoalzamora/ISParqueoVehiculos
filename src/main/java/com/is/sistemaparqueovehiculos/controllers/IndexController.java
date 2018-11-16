c7a18b001223a63f9e21d4bbfc61d9ff2be0ec74
Ricardo Alzamora Valle <alzamoraricardo5@gmail.com>
2018-11-15 22:02:50 -0500

    Se agrega ruta del controlador de incio [index].

--- src/main/java/com/is/sistemaparqueovehiculos/controllers/IndexController.java
+++ src/main/java/com/is/sistemaparqueovehiculos/controllers/IndexController.java
@@ -1,13 +1,15 @@
 package com.is.sistemaparqueovehiculos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @RequestMapping("/")
-    public String inicio(){
+    public String inicio(Model model){
+        model.addAttribute("titulo", "Inicio");
         return "index";
     }
 }
