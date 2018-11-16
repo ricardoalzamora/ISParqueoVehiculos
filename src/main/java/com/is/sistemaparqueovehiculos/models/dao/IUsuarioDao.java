1d6af859a6f006daecff62f6d75ceb809b29bcc0
Ricardo Alzamora Valle <alzamoraricardo5@gmail.com>
2018-11-15 22:06:17 -0500

    Limpieza de código fuente.

--- src/main/java/com/is/sistemaparqueovehiculos/models/dao/IUsuarioDao.java
+++ src/main/java/com/is/sistemaparqueovehiculos/models/dao/IUsuarioDao.java
@@ -4,13 +4,7 @@ import com.is.sistemaparqueovehiculos.models.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository("usuariodao")
 public interface IUsuarioDao extends JpaRepository<Usuario, Long> {
-
-    public abstract List<Usuario> findByNombre(String nombre);
-
-    public abstract Usuario findByDocumento(Long documento);
 
 }
